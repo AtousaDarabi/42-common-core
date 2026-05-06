5ac72e10cb786a30777a1f3bc3355102fe3c4d33
Atousa Darabi <adarabi@3-A-23.42heilbronn.de>
2026-05-07 00:35:31 +0200

    RRemoved duplicated header

--- libft/ft_calloc.c
+++ libft/ft_calloc.c
@@ -6,12 +6,11 @@
 /*   By: adarabi <adarabi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/27 19:26:23 by adarabi           #+#    #+#             */
-/*   Updated: 2026/05/07 00:25:41 by adarabi          ###   ########.fr       */
+/*   Updated: 2026/05/07 00:34:12 by adarabi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 #include <stdlib.h>
 
 void	*ft_calloc(size_t mem_count, size_t size)
