f8a9b642e9c562dbedf25d9237fe0cf917d2eb20
Atousa Darabi <adarabi@3-A-25.42heilbronn.de>
2026-05-18 22:13:44 +0200

    Added get_next_line.

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adarabi <adarabi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/18 18:51:44 by adarabi           #+#    #+#             */
+/*   Updated: 2026/05/18 22:10:38 by adarabi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+void	ft_strlcpy(char *dst, char *src, size_t size);
+size_t	ft_strlen(char *str);
+int		ft_chrfind(char *str, int c);
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*fileread(int fd, char *s);
+char	*get_first_line(char *s);
+
+#endif
