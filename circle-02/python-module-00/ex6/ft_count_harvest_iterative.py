85247ae4a0b573abc5e6aca1ea690d912e4a7610
AtousaDarabi <atousadarabi@gmail.com>
2026-06-16 22:16:01 +0200

    Fixed bug in python 00 ex6.

--- circle-02/python-module-00/ex6/ft_count_harvest_iterative.py
+++ circle-02/python-module-00/ex6/ft_count_harvest_iterative.py
@@ -1,6 +1,6 @@
 def ft_count_harvest_iterative():
     n = int(input("Days until harvest: "))
-    for i in range(1, n):
+    for i in range(1, n + 1):
         print("Days ", i)
     print("Harvest time!")
 
