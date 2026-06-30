5b9b57350986b301886517ffd733e0e3c9a79806
AtousaDarabi <atousadarabi@gmail.com>
2026-06-30 12:40:33 +0200

    Added ex7.

---
+++ circle-02/python-module-00/ex7/ft_seed_inventory.py
@@ -0,0 +1,14 @@
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+    name = seed_type.capitalize()
+    match unit:
+        case "packets":
+            string= f"{name} seeds: {quantity} {unit} available"
+        case "grams":
+            string= f"{name} seeds: {quantity} {unit} available"
+        case "area":
+            string= f"{name} seeds: covers {quantity} square meters"
+    print(string)
+
+# ft_seed_inventory("tomato", 15, "packets")
+# ft_seed_inventory("carrot", 8, "grams")
+# ft_seed_inventory("lettuce", 12, "area")
