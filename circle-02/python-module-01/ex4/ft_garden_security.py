c07bbd963714ca6cdf9df3d8676fecb7fc5c8548
AtousaDarabi <atousadarabi@gmail.com>
2026-07-09 22:09:07 +0200

    Updated python module 01.

--- circle-02/python-module-01/ex4/ft_garden_security.py
+++ circle-02/python-module-01/ex4/ft_garden_security.py
@@ -2,38 +2,62 @@
 
 class Plant:
 
-    def __init__(self, name, height, age) -> None:
-        self.name = name
-        self.height = height
-        self.age = age
+    def __init__(self, name: str, height: float, age: int) -> None:
+        self._name = name
 
-    def show(self) -> None:
-        days = "day" if self.age == 1 else "days"
-        print(f"Created: {self.name.capitalize()}: {round(self.height, 1)}cm, {self.age} {days} old")
+        if height >= 0:
+            self._height = float(height)
+        else:
+            print(f"{self._name}: Height can't be negative")
+            self._height = 0.0
+
+        if age >= 0:
+            self._age = age
+        else:
+            print(f"{self._name}: Age can't be negative")
+            self._age = 0
+        
+        print(f"Plant created: {self._name}: {self._height}cm, {self._age} days old")
+
+
+    # getter
+    def get_height(self) -> float:
+        return self._height
+
+    def get_age(self) -> int:
+        return self._age
+    # getter
     
-    def grow_plant(self) -> None:
-        growth = 0.8
-        self.height += growth
+    # setter
+    def set_height(self, value: float):
+        if value >= 0:
+            print(f"{self._name}: Height updated: {value}cm")
+            self._height = value
+        else:
+            print(f"{self._name}: Error, height can't be negative")
+            print(f"Height update rejected")
     
-    def age_plant(self) -> None:
-        self.age += 1
-    
-    def age_grow_plant(self) -> None:
-        self.grow_plant()
-        self.age_plant()
+    def set_age(self, value: int):
+        if value >= 0:
+            print(f"Age updated: {value} days")
+            self._age = value
+        else:
+            print(f"{self._name}: Error, age can't be negative")
+            print(f"Age update rejected")
+    # setter
+
+    def print_status(self):
+        print(f"Current state: {self._name}: {self._height:.1f}cm, {self._age} days old")
 
 def main() -> None:
     i = 1
     print("=== Garden Security System ===")
-    plant = {}
-    plant["rose"] = Plant("rose", 25.0, 30)
-    plant["oak"] = Plant("oak", 200.0, 365)
-    plant["cactus"] = Plant("cactus", 5.0, 90)
-    plant["sunflower"] = Plant("sunflower", 80.0, 40)
-    plant["fern"] = Plant("fern", 15.0, 120)
-
-    for p in plant.values():
-        p.show()
+    rose = Plant("Rose", 15.0, 10)
+    rose.set_height(25)
+    rose.set_age(30)
+    rose.set_height(-5)
+    rose.set_age(-10)
+    rose.display_status()
 
 if __name__ == "__main__":
     main()
